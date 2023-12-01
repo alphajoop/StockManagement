97c01f55cdbc4ea27f767b791f519a8bc3a47125
Alpha DIOP <alphakhoss@gmail.com>
2023-12-01 15:57:45 +0000

    fix0: auth system integration

--- controllers/userController.js
+++ controllers/userController.js
@@ -100,3 +100,26 @@ exports.updateUserRole = async (req, res) => {
       .json({ message: 'Failed to update user role', error: error.message });
   }
 };
+
+// Get details of all users
+exports.getAllUsers = async (req, res) => {
+  try {
+    // Check if the authenticated user is an admin
+    if (req.auth.role !== 'admin') {
+      return res
+        .status(403)
+        .json({ message: 'Forbidden: Insufficient permissions' });
+    }
+
+    // Fetch all users from the database
+    const users = await User.find({}, { password: 0 }); // Exclude password field
+
+    // Return the details of all users
+    res.status(200).json(users);
+  } catch (error) {
+    console.error('Error fetching all users:', error);
+    res
+      .status(500)
+      .json({ message: 'Failed to fetch all users', error: error.message });
+  }
+};
--- routes/userRouter.js
+++ routes/userRouter.js
@@ -8,6 +8,9 @@ const isAdmin = require('../middlewares/isAdminMiddleware');
 // Update user role (admin action)
 router.put('/update-role', auth, isAdmin, userController.updateUserRole);
 
+// Get details of all users (admin action)
+router.get('/all-users', auth, isAdmin, userController.getAllUsers);
+
 router.post('/signup', userController.signup);
 router.post('/login', userController.login);
 
